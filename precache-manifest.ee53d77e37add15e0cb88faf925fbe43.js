34d578817c4f23605c6ac59dbcf6d59d6ea9bd87
Ankit Gond <82073616+AnkitG7@users.noreply.github.com>
2022-05-06 15:23:35 +0530

    Updates

---
+++ precache-manifest.ee53d77e37add15e0cb88faf925fbe43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4d411c46f6d8c0f4b41",
+    "url": "/TextUtils-React-Js/static/css/main.d189138b.chunk.css"
+  },
+  {
+    "revision": "e4d411c46f6d8c0f4b41",
+    "url": "/TextUtils-React-Js/static/js/main.0c97e2ce.chunk.js"
+  },
+  {
+    "revision": "3e276bde1fa44825bffe",
+    "url": "/TextUtils-React-Js/static/js/runtime~main.eaa4e529.js"
+  },
+  {
+    "revision": "bb21798b792c99b8ae9a",
+    "url": "/TextUtils-React-Js/static/js/2.8b9ca286.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/TextUtils-React-Js/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "83dfcb2d651873f90bd004a3888036b6",
+    "url": "/TextUtils-React-Js/index.html"
+  }
+];
